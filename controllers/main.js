07a845c1b4769adac7dfe6e539c5136125d8929e
othmaneuw <strangeothmane@gmail.com>
2022-12-31 15:24:00 +0100

    verifying token & auth middleware

--- controllers/main.js
+++ controllers/main.js
@@ -1,23 +1,30 @@
-const CustomAPIError = require('../errors/custom-error');
-const jwt = require('jsonwebtoken');
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
-const login = async (req,res) =>{
-    const {username,password} = req.body;
-    if(!username || !password){
-        throw new CustomAPIError('Please provide username and password',400);
-    }
-    const id = new Date().getDate();
-    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'});
-    res.status(200).json({msg :'user created',token});
-}
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide username and password", 400);
+  }
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
+};
 
-const dashboard = async (req,res)=>{
-    const luckyNumber = Math.floor(Math.random()*100);
-    res.status(200).json({msg : 'Hello Othmane',secret : `Here is your secret Number ${luckyNumber}`});
-}
-
+const dashboard = async (req, res) => {
+   
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res
+    .status(200)
+    .json({
+      msg: `Hello ${req.user.username}`,
+      secret: `Here is your secret Number ${luckyNumber}`,
+    });
+};
 
 module.exports = {
-    login,
-    dashboard
+  login,
+  dashboard,
 };
