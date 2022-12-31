6343b3ec5faae66d6790a90ff9b610bbbb5d917f
othmaneuw <strangeothmane@gmail.com>
2022-12-31 16:03:47 +0100

    refactoring errors

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error');
+const BadRequestError = require('./bad-request');
+const UnauthenticatedError = require('./unautenticated');
+
+module.exports = {
+    CustomAPIError,
+    BadRequestError,
+    UnauthenticatedError
+};
